2fcebc59b39704f8b65dcb1ee44a2398e8505663
Eoic <karolis.strazdas@protonmail.com>
2025-03-14 21:49:26 +0200

    Project template init.

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 """
-    Setup file for pointersync.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 4.6.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
+Setup file for pointersync.
+Use setup.cfg to configure your project.
 """
 
 from setuptools import setup
@@ -12,7 +8,7 @@ from setuptools import setup
 if __name__ == "__main__":
     try:
         setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
+    except:
         print(
             "\n\nAn error occurred while building the project, "
             "please ensure you have the most updated version of setuptools, "
